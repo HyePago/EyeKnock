77b3fbedd7f00a235ebe159a15a6d26c6a5a4cb9
sunwonsara <sunwonsara@naver.com>
2018-03-31 16:46:40 +0900

    Eyeknock 최종 커밋

---
+++ README.md
@@ -0,0 +1,1 @@
+"# EyeKnock" 
---
+++ app/src/main/java/com/example/ip/eyeknock/Activity2.java
@@ -0,0 +1,31 @@
+package com.example.ip.eyeknock;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
